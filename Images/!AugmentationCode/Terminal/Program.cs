b63a928ad6028c1b66356b3c1fe7b0c705222b71
Semrah Odobasic <semrahodobasic@icloud.com>
2024-04-18 23:47:06 -0600

    Updated images and added documentation

--- Images/!AugmentationCode/Terminal/Program.cs
+++ Images/!AugmentationCode/Terminal/Program.cs
@@ -6,8 +6,8 @@ namespace Terminal
     {
         static void Main(string[] args)
         {
-            var inputDirectory = "E:/!!IRL/School/!!University/UofU/ECE5960/veinfinder/Images/Training Images/Input";
-            var outputDirectory = "E:/!!IRL/School/!!University/UofU/ECE5960/veinfinder/Images/Training Images/Annotations";
+            var inputDirectory = "E:/!!IRL/School/!!University/UofU/ECE5960/veinfinder/Images/Original Image Set/Input";
+            var outputDirectory = "E:/!!IRL/School/!!University/UofU/ECE5960/veinfinder/Images/Original Image Set/Annotations";
 
             var imageAugmentor = new AugmentationObject();
             imageAugmentor.AugmentImages(inputDirectory, outputDirectory);
